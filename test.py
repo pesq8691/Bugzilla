f77e99321a1b546434cbd8f8d328ec60ce897a09
pesq8691 <dpesqueda@csumb.edu>
2016-03-02 15:49:09 -0800

    Edited Python Code

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 import urllib
 import re
 import json
+import cgi
+import sys
 
 # insert any Bugzilla url here and run
 # the results are pushed out to a text file named Output.txt
 # we need to make this script able to accept data sent from the js file
 # it should also be able to send back the ID's as an array or a json obj
-url = ''
+fs = cgi.FieldStorage()
+
+sys.stdout.write("Content-Type: application/json")
+
+sys.stdout.write("\n")
+sys.stdout.write("\n")
+
+
+result = {}
+url = fs['url']
 htmlFile = urllib.urlopen(url)
 htmlText = htmlFile.read()
 
 data = htmlText.split('<a href="show_bug.cgi?id=')
 
-text_file = open("Output.txt", "w")
+#text_file = open("Output.txt", "w")
 item = 1;
 for x in range(1, len(data)):
     myString = re.compile('([0-9]+)(">)([0-9]+)(</a>)')
@@ -20,9 +31,12 @@ for x in range(1, len(data)):
     if(len(myObject) > 0):
         print item, ' ', myObject[0][0]
         myItem = myObject[0][0] + ' '
-        text_file.write(myItem)
+        result[item]+=myItem
         item += 1
 
-text_file.close()
+sys.stdout.write(json.dumps(result,indent=1))
+sys.stdout.write("\n")
+
+sys.stdout.close()
 
 
